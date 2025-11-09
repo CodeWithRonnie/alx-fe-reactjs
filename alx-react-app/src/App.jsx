672810d0f4f0fe4964f61f91e7fb262dc663fed9
mamikie maemu <maemu.ronewa98@gmail.com>
2025-11-09 21:25:58 +0200

    Added UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 
@@ -17,6 +19,7 @@ function App() {
        <WelcomeMessage />
        <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
