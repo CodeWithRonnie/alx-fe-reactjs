b0a224b0b806d7db670a87ea11899f84a26decf4
mamikie maemu <maemu.ronewa98@gmail.com>
2025-11-16 21:42:16 +0200

    move Navbar.jsx to components folder and ensure correct imports in App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
