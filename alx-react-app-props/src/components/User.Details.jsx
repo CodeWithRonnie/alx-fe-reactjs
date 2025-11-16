77ddebf88f63674d5067f8f508e61b991983e47c
mamikie maemu <maemu.ronewa98@gmail.com>
2025-11-16 21:04:27 +0200

    moved components to src/components and implemented Context API for user data

---
+++ alx-react-app-props/src/components/User.Details.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import UserContext from "../UserContext";
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "15px",
+        margin: "15px",
+        borderRadius: "8px",
+        maxWidth: "400px",
+      }}
+    >
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
