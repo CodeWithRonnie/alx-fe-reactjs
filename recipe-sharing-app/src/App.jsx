f0886af06bc5d641f108f7f4bc557af3ac6800c3
mamikie maemu <maemu.ronewa98@gmail.com>
2025-11-16 20:27:42 +0200

    wrap App.jsx with Router to satisfy routing check

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,24 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import RecipeList from "./components/RecipeList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 import RecipeDetail from "./components/RecipeDetail";
+import EditRecipeForm from "./components/EditRecipeForm";
+import DeleteRecipeButton from "./components/DeleteRecipeButton";
 
 function App() {
   return (
-    <>
+    <Router>
       <h1>Recipe Sharing App</h1>
-
       <Routes>
-        <Route path="/" element={
-          <>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        } />
-
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
+        <Route path="/recipe/:id/delete" element={<DeleteRecipeButton />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
