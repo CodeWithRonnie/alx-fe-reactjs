80859342f1fd7f7b8bee1acd1000fb01064ffb20
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-11-30 16:15:19 +0200

    Added  advanced Interactivity and Transitions with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,10 +3,12 @@ function UserProfile() {
     <div
       className="
         bg-gray-100 
-        p-4 sm:p-4 md:p-8 
-        max-w-xs sm:max-w-xs md:max-w-sm
+        p-4 sm:p-6 md:p-8 
+        max-w-xs sm:max-w-sm 
         mx-auto my-20 
-        rounded-lg shadow-lg
+        rounded-lg shadow-lg 
+        transition-shadow duration-300 
+        hover:shadow-xl
       "
     >
       <img
@@ -14,33 +16,30 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full 
-          w-24 h-24 
-          sm:w-24 sm:h-24 
-          md:w-36 md:h-36 
-          mx-auto
+          w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 
+          mx-auto 
+          transition-transform duration-300 ease-in-out
+          hover:scale-110
         "
       />
 
       <h1
         className="
-          text-lg 
-          sm:text-lg 
-          md:text-xl 
+          text-lg sm:text-xl md:text-2xl 
           text-blue-800 
-          my-4
+          my-4 text-center 
+          transition-colors duration-300
+          hover:text-blue-500
         "
       >
-        John Doe
+    Mamikie Maemu
       </h1>
 
-      <p
-        className="
-          text-sm 
-          sm:text-sm 
-          md:text-base 
-          text-gray-600
-        "
-      >
+      <p className="
+        text-sm sm:text-base 
+        text-gray-600 
+        text-center
+      ">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
