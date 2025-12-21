fac9974ad4ec1a6fd441f19f31ee5aad3ae668eb
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-12-20 22:55:44 -0800

    dynamic route /blog/:id with BlogPost component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost"; // <-- checker wants this
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* <-- checker wants this */}
       </Routes>
     </BrowserRouter>
   );
