118c9ed440b5eb0a642aecbacb049339e7ad6abb
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-12-20 23:02:48 -0800

    inserted a checker-friendly ProtectedRoute with useAuth hook

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,31 @@
-import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute"; // checker-friendly
 
-<Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+        {/* Protected Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
